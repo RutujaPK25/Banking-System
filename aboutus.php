fcb5031b8f166d7a47f6aae334f83f18bffc68d3
Rutuja Kothari <rutujakothari25@gmail.com>
2021-07-17 20:34:57 +0530

    Basic Banking System

---
+++ aboutus.php
@@ -0,0 +1,61 @@
+<?php
+  include ('nav.php');
+?>
+
+    <title>About Us</title>
+    <style>
+        h1{
+            background-color:rgb(201, 238, 241);
+            text-align: center;
+        }
+        h1:hover{
+    background-color: rgb(102, 113, 114);
+    color:black;
+    transition-duration: 1s;
+}
+        p{
+            text-align: left;
+            font-size: 20px;
+        }
+       
+        h2{
+            background-color: rgb(201, 238, 241);
+            text-align: center;
+            
+        }
+        h2:hover{
+    background-color: rgb(102, 113, 114);
+    color:black;
+    transition-duration: 1s;
+}
+    </style>
+</head>
+<body>
+    <h1>WISDOM BANK</h1>
+    <p>When it comes to discount broking, a newer version of online stock trading, Wisdom Capital has created distinct niche for itself. Ever since its inception, Wisdom Capital is  
+        Based at Hi-Tech city, Noida, Uttar Pradesh, Wisdom Capital is run as an online brand of Ashlar Securities (incorporated in 2016). In the new age technology driven, Wisdom Capital has plans to carve out its special place in the industry by focusing on mobile technologies that will allow ease of use, prompt execution of traders, and real-time interactive data tools. 
+        Wisdom Capital’s passion for technology and innovation will not help to tread an uncharted territory but also give Wisdom Capital a chance to create a technology driven customer centric company in the near future: one that will be unmatched in the industry.
+        As your financial partner for life, we believe it is our responsibility to gather, develop, share and apply wisdom to help you make the best possible decisions to ensure your financial success.<br>
+      <span>  <h2>OUR MISSION</h2></span>
+      <p> Wisdom Capital aims to emerge as a leading discount stock brokerage firm offering robust client centric
+        technology enabled trading/investment solutions for efficient and quick transaction while creating a market aware environment.</p><br>
+     <span> <h2>OUR VISION</h2>  </span>
+<p>Wisdom Capital aspires to be a leading discount brokerage firm creating an environment of financial literacy and offering 
+the most resourceful trading tools to make it the most efficient and prompt trading experience ever for its esteemed clients.</p><br>
+    
+    
+</p>
+<span><h2>OUR OFFERINGS</h2></span>
+<p>Express FD, Personal Loan, Car Loan, Savings Account, 24x7 Loans, Credit Card, Health Insurance, FD Interest Rate, Fastag, Current account, 
+    Internet Banking</p><br>
+<h2>SERVICES</h2>
+<p>Avail the Best Discount Brokerage Services by Wisdom Capital
+As a leading online stock broker and precursor of discount brokerage in India, Wisdom Capital offers an exclusive array of trading facilities and services to its esteemed clients across different market segments. The range of trading facilities offered by Wisdom Capital is as follows:
+
+Equity (Stock) Trading
+We offer online trading facilities in equities, catering to the needs of individual Indian and Non-Resident Indian (NRI) retail investors. Wisdom Capital offers broker assisted trade execution and automated online investment in all stocks in BSE and NSE. In order to facilitate a trader to take a guided decision, we offer advisories by seasoned investment professionals to invest or trade in stocks. For an expert investor like you,
+ we offer live Wisdom Data at a minimal price to research into a stock and further trade or invest it.
+</p>
+<?php include('footer.php'); ?>
+</body>
+</html>
